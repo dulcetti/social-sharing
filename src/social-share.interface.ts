8f9cd819f197bb20d1e5e31dd73a5018ba4a0e5e
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-21 23:25:59 -0300

    refactor: adapt component to microbundle - add eslint - add initial interface - add describe to test - update declaration and props on component - add typings

---
+++ src/social-share.interface.ts
@@ -0,0 +1,3 @@
+export interface ISocialShare {
+  title?: string;
+}
--- src/social-share.test.tsx
+++ src/social-share.test.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { SocialShare } from './social-share';
 
-test('render the title default', () => {
-  const { getByText } = render(<SocialShare />);
-  const titleDefault = getByText(/Gostou\? Compartilhe esse artigo ;\)/i);
-  expect(titleDefault).toBeInTheDocument();
-});
+describe('Social Share Component', () => {
+  test('is truthy', () => {
+    expect(SocialShare).toBeTruthy();
+  });
 
-test('render the title prop', () => {
-  const { getByText } = render(<SocialShare title="Fodasse" />);
-  const textTitleProp = getByText(/Fodasse/i);
-  expect(textTitleProp).toBeInTheDocument();
+  test('render the title default', () => {
+    const { getByText } = render(<SocialShare />);
+    const titleDefault = getByText(/Gostou\? Compartilhe esse artigo ;\)/i);
+    expect(titleDefault).toBeInTheDocument();
+  });
+
+  test('render the title prop', () => {
+    const { getByText } = render(<SocialShare title='Fodasse' />);
+    const textTitleProp = getByText(/Fodasse/i);
+    expect(textTitleProp).toBeInTheDocument();
+  });
 });
