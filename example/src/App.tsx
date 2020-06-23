68df41edb110e2a69233f9d7a27d0d8b3cc3e043
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-22 22:02:53 -0300

    feat: remove example from ignore

---
+++ example/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { SocialShare } from 'social-sharing';
+
+const App = () => {
+  return <SocialShare title='Create React Library Example 😄' />;
+};
+
+export default App;
