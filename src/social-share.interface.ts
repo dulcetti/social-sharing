f05379d28a008c748c8a9899d12b68fa85785746
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-23 22:57:53 -0300

    feat: add all fileds on interface

--- src/social-share.interface.ts
+++ src/social-share.interface.ts
@@ -1,3 +1,9 @@
-export interface ISocialShare {
+export interface PropsSocialShare {
+  author?: string;
+  className?: string;
+  textButton?: string;
   title?: string;
+  url: string;
+  urlDescription?: string;
+  urlTitle: string;
 }
