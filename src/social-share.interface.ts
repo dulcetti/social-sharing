fb2294adf91e82524fcf883c9499a6b7000597a9
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-26 23:33:04 -0300

    Svgs on template (#10)
    * refactor: remove svgs component file
    
    * feat: icons on component
    - add svgs on social
    - add initial options prop
    
    * feat: remove icons interface and add others

--- src/social-share.interface.ts
+++ src/social-share.interface.ts
@@ -1,6 +1,6 @@
 export interface PropsSocialShare {
-  author?: string;
   className?: string;
+  options?: PropsSvg;
   textButton?: string;
   title?: string;
   url: string;
@@ -8,11 +8,25 @@ export interface PropsSocialShare {
   urlTitle: string;
 }
 
-export interface PropsIcons {
-  className?: string;
-  name: string;
-}
-
 export interface PropsSvg {
   label?: string;
+  social?: SocialMedias;
+  text?: string;
+}
+
+interface SocialMedias {
+  author?: string;
+  facebook?: Colors;
+  linkedin?: Colors;
+  share?: {
+    circle?: string;
+    polygon?: string;
+  };
+  twitter?: Colors;
+  whatsapp?: Colors;
+}
+
+interface Colors {
+  bg?: string;
+  color?: string;
 }
