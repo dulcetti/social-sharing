eb4b3b25cc4b31a9373036b7bfbfe71df0550b8d
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-27 11:55:47 -0300

    feat: add options an example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 import SocialShare from 'social-sharing';
 
 const App = () => {
+  const optionsExample = {
+    author: 'dulcetti',
+    bgWhatsapp: 'red',
+  };
+
   return (
     <>
       <SocialShare
+        options={optionsExample}
         url='https://www.brunodulcetti.com'
         urlTitle='Bruno Dulcetti'
         title='Create React Library Example 😄'
